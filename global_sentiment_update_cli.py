30a570ec385365a583f4090025929ce157579cf3
MiguelPeralvo <miguel.peralvo@gmail.com>
2018-09-07 19:56:10 +0100

    We insert 2 rows, for different sentiment types (although equal values at the momment).

--- global_sentiment_update_cli.py
+++ global_sentiment_update_cli.py
@@ -38,12 +38,22 @@ def update_impact_in_db(posts_to_update: List[Tuple[int, float]], use_ssh, db_ho
 
 
 def insert_current_global_sentiment_in_db(use_ssh, db_host, db_user, db_password, db_port, database_name, ssh_username, ssh_password):
-    sql_for_insert = """INSERT INTO analysis_global_sentiment(sentiment_type, created_at_epoch_ms, sentiment_absolute)
+    # For the time being, we'll equate stocktwits to social media
+
+    sql_for_insert = """
+        INSERT INTO analysis_global_sentiment(sentiment_type, created_at_epoch_ms, sentiment_absolute)
         (SELECT 'social_media', UNIX_TIMESTAMP(NOW())*1000, SUM(impact*(sentiment_mixed-0.5)) 
         FROM analysis_posts_sentiment 
-        WHERE created_at_epoch_ms >=(SELECT UNIX_TIMESTAMP(NOW())*1000-(1*3600*1000)));"""
+        WHERE created_at_epoch_ms >=(SELECT UNIX_TIMESTAMP(NOW())*1000-(1*3600*1000)));
+        
+        INSERT INTO analysis_global_sentiment(sentiment_type, created_at_epoch_ms, sentiment_absolute)
+        (SELECT 'stocktwits', UNIX_TIMESTAMP(NOW())*1000, SUM(impact*(sentiment_mixed-0.5)) 
+        FROM analysis_posts_sentiment 
+        WHERE created_at_epoch_ms >=(SELECT UNIX_TIMESTAMP(NOW())*1000-(1*3600*1000)));
+        
+        """
 
-    db_utils.update(use_ssh, sql_for_insert, db_host, db_user, db_password, db_port, database_name, ssh_username, ssh_password, multi=False)
+    db_utils.update(use_ssh, sql_for_insert, db_host, db_user, db_password, db_port, database_name, ssh_username, ssh_password, multi=True)
 
 def compute_impact(post):
     return 1 + post.get('conversation_replies', 0) + post.get('likes_total', 0)
