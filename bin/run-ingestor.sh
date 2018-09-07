5fb642af30699fbb82ea4e62a0b59a2917025a56
MiguelPeralvo <miguel.peralvo@gmail.com>
2018-09-07 19:43:19 +0100

    Fixed typo.

--- bin/run-ingestor.sh
+++ bin/run-ingestor.sh
@@ -31,5 +31,5 @@ sleep 45
 cd ${DIR}/..
 nohup sh -c "python3.6 filter_posts_for_predict_cli.py -idf $KCL_OUTPUT_FILE -ssh | python3.6 predict_cli.py -if $STOCKTWITS_SENTIMENT_ITOS_MODEL_PATH -tcf $STOCKTWITS_SENTIMENT_MODEL_PATH  | python3.6 db_ingestor_cli.py -ssh" 2> ${DATA_DIR}/${STDERR_SENTIMENT_INFERENCE_FILE} &
 
-nohup sh -c "python3.6 global_sentiment_update.py -ssh " 2> ${DATA_DIR}/${STDERR_GLOBAL_SENTIMENT_UPDATE_FILE} &
+nohup sh -c "python3.6 global_sentiment_update_cli.py -ssh " 2> ${DATA_DIR}/${STDERR_GLOBAL_SENTIMENT_UPDATE_FILE} &
 
